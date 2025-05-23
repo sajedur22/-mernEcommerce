8b17911cd59c6fa8dabb6077c0b0fc965b3f676f
sajedur22 <151081528+sajedur22@users.noreply.github.com>
2025-05-23 19:24:38 +0600

    jwt
    jwt token

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,20 +1,24 @@
 const EmailSend = require("../utility/EmailHelper");
-const UserModel = require("../models/UserModel");
+const UserModel=require("../models/UserModel")
 
 
-const UserOTPService=async (req)=>{
-    try{
+
+
+const UserOTPService = async (req) => {
+    try {
         let email=req.params.email;
         let code=Math.floor(100000+Math.random()*900000);
+
         let EmailText=`Your Verification Code is= ${code}`
         let EmailSubject='Email Verification'
-        await EmailSend(email,EmailText,EmailSubject)
-        await UserModel.updateOne({email:email},{$set:{otp:code}},{upsert:true})
-        return {status:"success",message:"6 Digit OTP Code sent"}
-    }
-    catch (e) {
-        return {status:"fail",message:"something Went Wrong"};
 
+        await EmailSend(email,EmailText,EmailSubject);
+
+        await UserModel.updateOne({email:email},{$set:{otp:code}},{upsert:true})
+
+        return {status:"success", message:"6 Digit OTP has been send"}
+    }catch (e) {
+        return {status:"fail", message:e}
     }
 }
 
--- src/utility/EmailHelper.js
+++ src/utility/EmailHelper.js
@@ -1,21 +1,24 @@
 const nodemailer=require('nodemailer');
-const {sendMail} = require("nodemailer/lib/mailer");
+
 const EmailSend=async (EmailTo,EmailText,EmailSubject)=>{
-    let transporter=nodemailer.createTransport({
-        host:"mail.teamrabbuil.com",
+
+    let  transport= nodemailer.createTransport({
+        host:"mail.teamrabbil.com",
         port:25,
         secure:false,
         auth:{user:"info@teamrabbil.com",pass:"~sR4[bhaC[Qs"},
         tls:{rejectUnauthorized:false}
     })
+
+
     let mailOption={
         from:'MERN Ecommerce Solution <info@teamrabbil.com>',
         to:EmailTo,
         subject:EmailSubject,
-        text:EmailText,
-
+        text:EmailText
     }
-    return await sendMail(mailOption);
+
+    return await transport.sendMail(mailOption)
 }
 
 module.exports=EmailSend;
