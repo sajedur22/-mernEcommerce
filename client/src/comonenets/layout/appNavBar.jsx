fbbe6df952e9e68231f0b2961ad0cda731a72897
sajedur22 <151081528+sajedur22@users.noreply.github.com>
2025-07-02 23:43:55 +0600

    navbar

--- client/src/comonenets/layout/appNavBar.jsx
+++ client/src/comonenets/layout/appNavBar.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import logo from '../../assets/images/plainb-logo.svg'
+import collapse from "bootstrap/js/src/collapse.js";
 
 const AppNavBar = () => {
     return (
@@ -26,6 +29,44 @@ const AppNavBar = () => {
                   </div>
               </div>
           </div>
+
+            <nav className={'navbar sticky-top bg-white navbar-expand-lg navbar-light py-3'}>
+                <div className={'container'}>
+                    <Link className={'navbar-brand'} to={'/'}>
+                        <img className={'img-fluid'} src={logo} alt={''} width={'95px'}/>
+                    </Link>
+                    <button className={'navbar-toggler'} type="button" data-bs-toggle={'collapse'} data-bs-target={"#nav06"} aria-controls={"nav06"} aria-expanded={'false'} aria-label={'Toggle navigation'} >
+                        <span className={'navbar-toggler-icon'}></span>
+                    </button>
+                    <div className={'collapse navbar-collapse'} id={'nav06'}>
+                        <ul className={'navbar-nav mt-3 mt-lg-0 mb-2 mg-lg-0 ms-lg-3'}>
+                            <span className={'nav-item me-4'}>
+                                <Link className={'nav-link'} to={'/'}>Home</Link>
+                            </span>
+                        </ul>
+                    </div>
+                    <div className={'d-lg-flex'}>
+                        <div className={'input-group'}>
+                            <input className={''} type={''} placeholder={'searce'} aria-label='searce'/>
+                            <button className={''}>
+                                <i className="bi bi-search"></i>
+                            </button>
+                            <Link to={'/cart'} type={'button'} className={' btn ms-2 btn-light position-relative '}>
+                                <i className="bi text-dark bi-bag"></i>
+                            </Link>
+                            <Link to={'/wish'} type={'button'} className={'btn ms-2 btn-light position-relative'}>
+                                <i className="bi text-dark bi-heart"></i>
+                            </Link>
+                            <Link to={'/profile'} type={'button'} className={'rounded-2 btn ms-3 btn-success d-flex'}>
+                                profile
+                            </Link>
+                            <Link to={'/profile'} type={'button'} className={'rounded-2 btn ms-3 btn-success d-flex'}>
+                                logout
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
